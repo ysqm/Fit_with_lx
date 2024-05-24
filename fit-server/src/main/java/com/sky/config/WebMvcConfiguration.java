95888224c6d6e7a246dd76c69c58a5a9d8d08fde
ysqm <411321639@qq.com>
2024-05-24 09:21:25 +0800

    Project environment

--- fit-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ fit-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -44,9 +44,9 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
     @Bean
     public Docket docket() {
         ApiInfo apiInfo = new ApiInfoBuilder()
-                .title("苍穹外卖项目接口文档")
+                .title("项目接口文档")
                 .version("2.0")
-                .description("苍穹外卖项目接口文档")
+                .description("项目接口文档")
                 .build();
         Docket docket = new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo)
--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -1,14 +1,55 @@
 CREATE DATABASE  IF NOT EXISTS `fit` ;
 USE `fit`;
 
-DROP TABLE IF EXISTS `Account_Mesagge`;
-CREATE TABLE `Account_Mesagge` (
-    `id`            bigint          NOT NULL AUTO_INCREMENT COMMENT '主键',
-    `user_id`       bigint          NOT NULL COMMENT '用户id',
-    `sex`           varchar(2)      COLLATE utf8_bin DEFAULT NULL COMMENT '性别',
-    `phone`         varchar(11)     COLLATE utf8_bin NOT NULL COMMENT '手机号',
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin COMMENT='地址簿';
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE users (
+                       id INT PRIMARY KEY AUTO_INCREMENT,
+                       account VARCHAR(50) NOT NULL,
+                       password VARCHAR(50) NOT NULL,
+                       avatar VARCHAR(100),
+                       username VARCHAR(50) NOT NULL,
+                       wechat_id VARCHAR(50),
+                       registration_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS `data`;
+CREATE TABLE data (
+                      id INT PRIMARY KEY AUTO_INCREMENT,
+                      text_content TEXT,
+                      string_content VARCHAR(255),
+                      timestamp_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                      is_expired INT DEFAULT 0,
+                      type INT,
+                      location VARCHAR(255)
+);
+
+DROP TABLE IF EXISTS `exercise_types`;
+CREATE TABLE exercise_types (
+                                id INT PRIMARY KEY,
+                                name VARCHAR(50)
+);
+
+DROP TABLE IF EXISTS `exercise_data`;
+CREATE TABLE exercise_data (
+                               id INT PRIMARY KEY AUTO_INCREMENT,
+                               type_id INT,
+                               start_time TIMESTAMP,
+                               end_time TIMESTAMP,
+                               calorie DOUBLE,
+                               FOREIGN KEY (type_id) REFERENCES exercise_types(id)
+);
+
+
+-- DROP TABLE IF EXISTS `Account_Message`;
+-- CREATE TABLE `Account_Message` (
+--     `id`            bigint          NOT NULL AUTO_INCREMENT COMMENT '主键',
+--     `user_id`       bigint          NOT NULL COMMENT '用户id',
+--     `sex`           varchar(2)      COLLATE utf8_bin DEFAULT NULL COMMENT '性别',
+--     `phone`         varchar(11)     COLLATE utf8_bin NOT NULL COMMENT '手机号',
+--     PRIMARY KEY (`id`)
+-- ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin COMMENT='地址簿';
+
+
 
 -- DROP TABLE IF EXISTS `Account_Mesagge`;
 -- CREATE TABLE `Account_Mesagge` (
